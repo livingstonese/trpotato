5b7c4af28fee321f9a22f5898e2d2aaf9d721e03
Livingstone SE <livingstone.se@flipkart.com>
2018-03-11 22:02:17 +0530

    Updated tests to ignore errors

--- crawler/parser_test.go
+++ crawler/parser_test.go
@@ -3,28 +3,23 @@ package crawler
 import "testing"
 import (
 	"github.com/livingstonese/trpotato"
-	"github.com/pkg/errors"
 )
 
 var testCases = []struct{
 	title string
 	movie *trpotato.Movie
-	err error
 }{
 	{
 		"Savarakathi (2018)[720p HDRip - x265 - HEVC - AC3 5.1 - 900MB - ESubs - Tamil]",
 		&trpotato.Movie{Title:"Savarakathi", Year:2018, Resolution:"720p", Size:"900MB"},
-		nil,
 	},
 	{
 		"Akilandakodi Brahmandanayagan (2018)[1080p v3 HD - AVC - MP4 - DD 5.1 - 6.4GB - ESubs - Tamil]",
 		&trpotato.Movie{Title:"Akilandakodi Brahmandanayagan", Year:2018, Resolution:"1080p", Size:"6.4GB"},
-		nil,
 	},
 	{
-		title: "TamilRockers Rare Movie Picks in True HD Special Thread Links",
-		movie: nil,
-		err: errors.New("Failed to parse movie"),
+		"Nee Enna Maayam Seidhai (2017) Tamil 720p HDRip x264 5.1 1.4GB",
+		&trpotato.Movie{Title:"Nee Enna Maayam Seidhai", Year:2017, Resolution:"720p", Size:"1.4GB"},
 	},
 }
 
@@ -36,12 +31,8 @@ func TestParse(t *testing.T) {
 			st.Logf("Testing %s", tc.title)
 			{
 				movie, err := parse(tc.title)
-				if tc.err != nil  {
-					return
-				}
-				if tc.err != err {
-					st.Errorf("Expected error value %v. Got %v", tc.err, err)
-					return
+				if err != nil  {
+					st.Errorf("Failed to parse movie")
 				}
 				st.Log("Successfully parsed movie.")
 				if movie.Title != tc.movie.Title {
